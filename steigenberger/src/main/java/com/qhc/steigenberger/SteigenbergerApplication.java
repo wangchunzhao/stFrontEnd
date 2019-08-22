f228e2f8df3b997c78fa4b1b2b18779289741f04
Wayne <wang@dxc.com>
2019-08-22 13:01:46 +0800

    init

---
+++ steigenberger/src/main/java/com/qhc/steigenberger/SteigenbergerApplication.java
@@ -0,0 +1,13 @@
+package com.qhc.steigenberger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SteigenbergerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SteigenbergerApplication.class, args);
+	}
+
+}
