7c342da3d6016549d9aec4298ca92400e40a167e
Wayne <wang@dxc.com>
2019-08-23 15:33:14 +0800

    update

---
+++ steigenberger/src/main/java/com/qhc/steigenberger/controller/UserController.java
@@ -0,0 +1,51 @@
+package com.qhc.steigenberger.controller;
+
+
+
+import java.io.IOException;
+import java.util.Date;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+//import io.swagger.annotations.Api;
+
+
+@Controller
+//@Api(value = "User Manager")
+public class UserController {
+
+	@RequestMapping(value = "index}", method = RequestMethod.GET)
+	public String  index() {
+
+		 return "redirect:/index.html";
+    }
+	@RequestMapping("login")
+	public String  main(Model model) {
+
+		 return "index";
+    }
+	@RequestMapping("hello")
+    public ModelAndView hello(){
+        ModelAndView modelAndView = new ModelAndView();
+        //1.存数据
+         modelAndView.addObject("name","name");
+         modelAndView.addObject("time",new Date());
+        //2.指定视图
+        modelAndView.setViewName("hello");
+        return modelAndView;
+    }
+	
+	@GetMapping(value = "home")
+    public void homePage(HttpServletResponse response)throws IOException{
+        response.sendRedirect("index.html");
+//        return "index";
+    }
+
+}
