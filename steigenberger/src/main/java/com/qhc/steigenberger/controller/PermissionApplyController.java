62aa1520bedd6377a8b653f3d80eac8d497bf52f
wangch <chunzhao.wang@carrier.com>
2019-08-29 11:48:10 +0800

    update

--- steigenberger/src/main/java/com/qhc/steigenberger/controller/PermissionApplyController.java
+++ steigenberger/src/main/java/com/qhc/steigenberger/controller/PermissionApplyController.java
@@ -21,12 +21,13 @@ public class PermissionApplyController {
 	RoleServiceI roleServiceImpl;
 
 	public static String BASE_URL = "http://127.0.0.1:8801/frye/";
-	public static String URL = "role/roleList/";
+	
 
 	@RequestMapping("/permissionApply")
 	public String roleList(Model model) {
 		
-	
+	 
+		
 		return "permission/permissionApply";
 	}
 	
