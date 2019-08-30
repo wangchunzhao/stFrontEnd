ded700e9cfdb77758b40fac1fc6c64a95d5f37bc
wangch <chunzhao.wang@carrier.com>
2019-08-30 13:59:16 +0800

    add

---
+++ steigenberger/src/main/resources/static/pageJs/permissionApply.js
@@ -0,0 +1,10 @@
+//提交表单
+function add(){
+	var username = document.getElementById('username').value;
+	var useremil = document.getElementById('useremil').value;
+	var userid = document.getElementById('userid').value;
+	var roleName = document.getElementById('roleName').value;
+	var area = document.getElementById('area').value;
+	var status = document.getElementById('status').value;
+	alert(status);
+}
