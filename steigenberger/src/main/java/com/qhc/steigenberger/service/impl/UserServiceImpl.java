3d7be5a03a642bdc888e6c4a9707a9988b797bcc
lizuoshan1028 <lizuoshan1028@163.com>
2019-08-30 09:52:30 +0800

    update

--- steigenberger/src/main/java/com/qhc/steigenberger/service/impl/UserServiceImpl.java
+++ steigenberger/src/main/java/com/qhc/steigenberger/service/impl/UserServiceImpl.java
@@ -9,12 +9,11 @@ import com.qhc.steigenberger.domain.Role;
 import com.qhc.steigenberger.domain.User;
 import com.qhc.steigenberger.service.UserServiceI;
 import com.qhc.steigenberger.service.WebServcieTool;
+import com.qhc.steigenberger.util.CommonConstant;
 
 @Service
 public class UserServiceImpl extends WebServcieTool<User> implements UserServiceI{
 	
-	static String BASEURL = "http://localhost:8801/frye/";
-
 	public PageInfo<User> selectAndPage(int pageNum, int pageSize, User user) {
 		PageHelper.startPage(pageNum, pageSize);
 		Integer isActive = user.getIsActive();
@@ -22,7 +21,7 @@ public class UserServiceImpl extends WebServcieTool<User> implements UserService
 		String userIdentity = user.getUserIdentity();
 		
 		String url = "user/findAll?userMail="+userMail+"&isActive="+isActive+"&userIdentity="+userIdentity;
-		List<User> list=findAll(BASEURL, url,User.class);
+		List<User> list=findAll(CommonConstant.BASEURL, url,User.class);
 		PageInfo<User> pageInfo=new PageInfo<>(list);
 		return pageInfo;
 	}
@@ -30,24 +29,24 @@ public class UserServiceImpl extends WebServcieTool<User> implements UserService
 	@Override
 	public User selectUserInfo(int id) {
 		
-		return findOne(BASEURL+"user/findById?id="+id,User.class);
+		return findOne(CommonConstant.BASEURL+"user/findById?id="+id,User.class);
 	}
 
 	@Override
 	public String saveUserInfo(User user) {
 		
-		return postInfo(user,BASEURL+"user/add",User.class);
+		return postInfo(user,CommonConstant.BASEURL+"user/add",User.class);
 	}
 	
 	@Override
 	public String updateUserInfo(User user) {
 		
-		return postInfo(user,BASEURL+"user/add",User.class);
+		return postInfo(user,CommonConstant.BASEURL+"user/add",User.class);
 	}
 	
 	@Override
 	public boolean delete(int id) {
-		String str = delete(BASEURL+"user/delete?id="+id);
+		String str = delete(CommonConstant.BASEURL+"user/delete?id="+id);
 		if("false".equals(str)) {
 			return false;
 		}else {
