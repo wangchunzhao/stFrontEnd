17b6e2c51de85b869babbfd0e31b77865b65b7db
wangch <chunzhao.wang@carrier.com>
2019-09-02 21:09:04 +0800

    update

--- steigenberger/src/main/java/com/qhc/steigenberger/service/impl/SapSalesOfficeServicelmpl.java
+++ steigenberger/src/main/java/com/qhc/steigenberger/service/impl/SapSalesOfficeServicelmpl.java
@@ -2,12 +2,14 @@ package com.qhc.steigenberger.service.impl;
 
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
 import com.qhc.steigenberger.domain.Role;
 import com.qhc.steigenberger.domain.SapSalesOffice;
 import com.qhc.steigenberger.service.SapSalesOfficeServicel;
 import com.qhc.steigenberger.service.WebServcieTool;
 import com.qhc.steigenberger.util.CommonConstant;
-
+@Service
 public class SapSalesOfficeServicelmpl extends WebServcieTool<SapSalesOffice> implements SapSalesOfficeServicel{
 
 	@Override
