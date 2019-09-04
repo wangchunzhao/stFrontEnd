06bd3c933dad6b48108fba00c98b15a153108acd
lizuoshan1028 <lizuoshan1028@163.com>
2019-09-04 17:42:06 +0800

    add

--- steigenberger/src/main/java/com/qhc/steigenberger/config/ApplicationConfig.java
+++ steigenberger/src/main/java/com/qhc/steigenberger/config/ApplicationConfig.java
@@ -1,10 +1,7 @@
 package com.qhc.steigenberger.config;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
 
 /**
  * @author wang@dxc.com
