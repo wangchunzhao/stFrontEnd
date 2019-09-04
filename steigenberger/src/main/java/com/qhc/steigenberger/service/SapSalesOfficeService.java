2ba3278308116e2af87c660541a126402a206519
lizuoshan1028 <lizuoshan1028@163.com>
2019-09-04 15:59:39 +0800

    update

--- steigenberger/src/main/java/com/qhc/steigenberger/service/SapSalesOfficeService.java
+++ steigenberger/src/main/java/com/qhc/steigenberger/service/SapSalesOfficeService.java
@@ -1,18 +1,19 @@
 package com.qhc.steigenberger.service;
 
 import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import com.qhc.steigenberger.domain.Role;
 import com.qhc.steigenberger.domain.SapSalesOffice;
-import com.qhc.steigenberger.service.WebServcieTool;
-import com.qhc.steigenberger.util.CommonConstant;
 @Service
-public class SapSalesOfficeService extends WebServcieTool<SapSalesOffice>{
+public class SapSalesOfficeService{
+	
+	@Autowired
+	FryeService<SapSalesOffice> fryeService;
+	
+	private final static String URL_SAPSALEOFFICE = "sapSalesOffice";
 
-	public List<SapSalesOffice> findAll() {
-		return  findAll(CommonConstant.BASEURL, "sapSalesOffice",SapSalesOffice.class);
+	public List<SapSalesOffice> getList() {
+		return  fryeService.getListInfo(URL_SAPSALEOFFICE,SapSalesOffice.class);
 	}
 
 }
