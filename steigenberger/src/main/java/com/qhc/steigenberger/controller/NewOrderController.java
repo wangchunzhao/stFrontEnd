424811cf5b1f9cb417b4ea25afb6b9db03d3f81d
wangch <chunzhao.wang@carrier.com>
2019-09-06 09:02:04 +0800

    update 1

--- steigenberger/src/main/java/com/qhc/steigenberger/controller/NewOrderController.java
+++ steigenberger/src/main/java/com/qhc/steigenberger/controller/NewOrderController.java
@@ -1,22 +1,33 @@
 package com.qhc.steigenberger.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.qhc.steigenberger.domain.JsonResult;
+
 @Controller
-@RequestMapping("/newOrder")
+@RequestMapping("newOrder")
 public class NewOrderController {
 	
 	
 	
 	@RequestMapping("/standardDiscount")
-	public ModelAndView permissionApply() {
-		ModelAndView modelAndView = new ModelAndView();
+	public String permissionApply() {
+		System.out.println("111111111");
 		
-		modelAndView.addObject("msg","对不起，您没有下订单权限!");
-		modelAndView.setViewName("newOrder/orderForm");
-		return modelAndView;
+		return "main";
+	}
+	
+	
+	
+	@PostMapping("/standardDiscount1")
+	@ResponseBody
+	public JsonResult permissionApply1() {
+		
+		return JsonResult.build(200,"success", 1);
 	}
 
 }
