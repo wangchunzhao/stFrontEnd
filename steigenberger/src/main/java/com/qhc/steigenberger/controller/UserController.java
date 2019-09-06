ee308e62eee4ee90f7810124c56c45a2bcab0fbf
lizuoshan1028 <lizuoshan1028@163.com>
2019-09-06 13:52:39 +0800

    updae

--- steigenberger/src/main/java/com/qhc/steigenberger/controller/UserController.java
+++ steigenberger/src/main/java/com/qhc/steigenberger/controller/UserController.java
@@ -1,30 +1,21 @@
 package com.qhc.steigenberger.controller;
 
-import java.io.IOException;
-import java.util.Date;
 import java.util.List;
-
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
 import com.github.pagehelper.PageInfo;
 import com.qhc.steigenberger.domain.JsonResult;
-import com.qhc.steigenberger.domain.RestPageUser;
 import com.qhc.steigenberger.domain.Role;
 import com.qhc.steigenberger.domain.User;
 import com.qhc.steigenberger.service.RoleService;
 import com.qhc.steigenberger.service.UserService;
-
-//import io.swagger.annotations.Api;
 
 @RequestMapping("user")
 @Controller
