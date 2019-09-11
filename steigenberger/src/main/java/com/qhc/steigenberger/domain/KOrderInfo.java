6da0289efff2907fe5c4130ffe742bf86df2caac
wangch <chunzhao.wang@carrier.com>
2019-09-11 14:45:59 +0800

    add 1

---
+++ steigenberger/src/main/java/com/qhc/steigenberger/domain/KOrderInfo.java
@@ -0,0 +1,111 @@
+package com.qhc.steigenberger.domain;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import com.qhc.steigenberger.util.Page;
+
+public class KOrderInfo extends Page implements Serializable {
+	
+	private int id;
+	
+	private String contractNo;
+	
+	private String contractUnit;
+	
+	private int area;
+	
+	private int orderType;
+	
+	private int b2c;
+	
+	private int specialDiscount;
+	
+	private Date createTime;
+	
+	private int status;
+	
+	private int sapStatus;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getContractNo() {
+		return contractNo;
+	}
+
+	public void setContractNo(String contractNo) {
+		this.contractNo = contractNo;
+	}
+
+	public String getContractUnit() {
+		return contractUnit;
+	}
+
+	public void setContractUnit(String contractUnit) {
+		this.contractUnit = contractUnit;
+	}
+
+	public int getArea() {
+		return area;
+	}
+
+	public void setArea(int area) {
+		this.area = area;
+	}
+
+	public int getOrderType() {
+		return orderType;
+	}
+
+	public void setOrderType(int orderType) {
+		this.orderType = orderType;
+	}
+
+	public int getB2c() {
+		return b2c;
+	}
+
+	public void setB2c(int b2c) {
+		this.b2c = b2c;
+	}
+
+	public int getSpecialDiscount() {
+		return specialDiscount;
+	}
+
+	public void setSpecialDiscount(int specialDiscount) {
+		this.specialDiscount = specialDiscount;
+	}
+
+	public Date getCreateTime() {
+		return createTime;
+	}
+
+	public void setCreateTime(Date createTime) {
+		this.createTime = createTime;
+	}
+
+	public int getStatus() {
+		return status;
+	}
+
+	public void setStatus(int status) {
+		this.status = status;
+	}
+
+	public int getSapStatus() {
+		return sapStatus;
+	}
+
+	public void setSapStatus(int sapStatus) {
+		this.sapStatus = sapStatus;
+	}
+	
+	
+}
---
+++ steigenberger/src/main/java/com/qhc/steigenberger/service/KOrderInfoService.java
@@ -0,0 +1,31 @@
+package com.qhc.steigenberger.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.qhc.steigenberger.domain.KOrderInfo;
+import com.qhc.steigenberger.util.PageHelper;
+
+@Service
+public class KOrderInfoService {
+	
+	@Autowired
+	FryeService<KOrderInfo> fryeService;
+	
+	@Autowired
+	FryeService<PageHelper> pageFryeService;
+	
+	private final static String URL_KORDERS = "kOrderInfo";
+	private final static String URL_KORDERS_LIST = "kOrderInfo/paging";
+	
+	public PageHelper<KOrderInfo> getList(int pageNum, int pageSize,KOrderInfo kOrderInfo){
+		
+		String contractNo = kOrderInfo.getContractNo()==null?"":kOrderInfo.getContractNo();
+		String contractUnit = kOrderInfo.getContractUnit()==null?"":kOrderInfo.getContractUnit();
+		
+		
+		String url = URL_KORDERS_LIST+"?pageNo="+pageNum+"&pageSize="+pageSize+"&contractNo="+contractNo+"&contractUnit="+contractUnit+"&b2c="+kOrderInfo.getB2c();
+		return pageFryeService.getInfo(url, PageHelper.class);
+	}
+
+}
