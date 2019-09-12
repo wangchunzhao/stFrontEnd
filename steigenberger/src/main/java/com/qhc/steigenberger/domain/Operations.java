fb76d3744eaca4efc630ea4a44e4e9eba40b2e2c
lizuoshan1028 <lizuoshan1028@163.com>
2019-09-12 09:33:42 +0800

    update

--- steigenberger/src/main/java/com/qhc/steigenberger/domain/Operations.java
+++ steigenberger/src/main/java/com/qhc/steigenberger/domain/Operations.java
@@ -13,7 +13,11 @@ public class Operations implements Serializable{
 	
     private String name;
     
+    private String description;
+    
     private boolean selected;
+    
+    
 	
 	
 	
@@ -39,6 +43,14 @@ public class Operations implements Serializable{
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
 	}
 
 
