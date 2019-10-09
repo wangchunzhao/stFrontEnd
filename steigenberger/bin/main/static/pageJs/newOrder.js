c6e2d5593515ee9c93312ee52845709d30b3f732
Songl <songl@dxc.com>
2019-10-09 14:51:07 +0800

    new order page update

--- steigenberger/bin/main/static/pageJs/newOrder.js
+++ steigenberger/bin/main/static/pageJs/newOrder.js
@@ -2,28 +2,42 @@
 $(function () {
     //1.初始化Table
 	var oTable = new TableInit();
-	$('#showGrossProfit').on('onclick',function(){
-		oTable.Init();
-	});
+	oTable.Init();
+	 $("#currency").change(function(value){
+		 if($(this).val()=='12'){
+			 $("#exchangeRate").val('6') 
+			 var exchangeRate = $("#exchangeRate").val()
+			 var contractAmount = $("#contractAmount").val();
+			 var orignalContractAmount = exchangeRate*contractAmount
+			 $("#orignalContractAmount").val(orignalContractAmount);
+		 }else{
+			 $("#exchangeRate").val('7')
+			 var exchangeRate = $("#exchangeRate").val()
+			 var contractAmount = $("#contractAmount").val();
+			 var orignalContractAmount = exchangeRate*contractAmount
+			 $("#orignalContractAmount").val(orignalContractAmount);
+		 }
+         
+     })
 
 });
-
+function selectOnchang(obj){
+	alert("");
+}
 var TableInit = function () {
-	var oTableInit = new Object();
-	
+	var oTableInit = new Object();	
 	oTableInit.Init = function () {
-		$('#mytab').bootstrapTable({
+		$('#customerTab').bootstrapTable({
 			method : 'get',
-			url : "/steigenberger/newOrder/getGrossProfitList",//请求路径
+			url : "",//请求路径
 			striped : true, //是否显示行间隔色
-			toolbar: '#toolbar',
 			cache: false,
 			pagination: true,                   //是否显示分页（*）
+			paginationDetailHAlign:'right',
 		    sortable: true,                     //是否启用排序
 		    clickToSelect: true,               //是否启用点击选中行
 		    sortOrder: "asc",                   //排序方式
 			pageNumber : 1, //初始化加载第一页
-			pagination : false,//是否分页
 			sidePagination : 'server',//server:服务器端分页|client：前端分页
 			pageSize : 30,//单页记录数
 			pageList : [ 10, 20, 30 ],//可选择单页记录数
@@ -34,28 +48,16 @@ var TableInit = function () {
 			    return temp;
 			},
 			columns : [ {
-				title : '产品名称流水号',
+				title : '签约单位',
 				field : 'id',
 				sortable : true
 			}, {
-				title : '金额',
+				title : '地址',
 				field : 'contractNo',
 				sortable : true
 			},{
-				title : '不含税金额',
+				title : '性质分类',
 				field : 'contractUnit',
-				sortable : true
-			},{
-				title : '成本',
-				field : 'area',
-				sortable : true
-			},{
-				title : '毛利',
-				field : 'orderType',
-				sortable : true
-			},{
-				title : '毛利率',
-				field : 'b2c',
 				sortable : true
 			}]
 		})
