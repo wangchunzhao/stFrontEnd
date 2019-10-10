c110b358345bdd9559b688f834a426237e1ebaf8
Songl <songl@dxc.com>
2019-10-10 13:13:10 +0800

    order page update

---
+++ steigenberger/src/main/resources/static/pageJs/commonJs.js
@@ -0,0 +1,14 @@
+document.write('<script src="../js/jquery-1.11.1.min.js"></script>');
+document.write('<script src="../js/modernizr.custom.js"></script>');
+document.write('<script src="../js/Chart.js"></script>');
+document.write('<script src="../js/metisMenu.min.js"></script>');
+document.write('<script src="../js/custom.js"></script>');
+document.write('<script src="../js/pie-chart.js" type="text/javascript"></script>');
+document.write('<script src="../js/classie.js"></script>');
+document.write('<script src="../js/jquery.nicescroll.js"></script>');
+document.write('<script src="../js/scripts.js"></script>');
+document.write('<script src="../js/SidebarNav.min.js" type="text/javascript"></script>');
+document.write('<script src="../js/SimpleChart.js"></script>');
+document.write('<script src="../js/bootstrap.js"> </script>');
+document.write('<script src="../bootstrap-table-master/dist/bootstrap-table.js" type="text/javascript"></script>');
+document.write('<script src="../bootstrap-table-master/dist/locale/bootstrap-table-zh-CN.js" charset="UTF-8" type="text/javascript"></script>');
