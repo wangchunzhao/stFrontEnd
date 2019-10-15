94a70563376512350d9251fd498b6f28c815b91d
Wayne <wang@dxc.com>
2019-10-15 17:29:09 +0800

    currency

--- steigenberger/src/main/java/com/qhc/steigenberger/domain/Currency.java
+++ steigenberger/src/main/java/com/qhc/steigenberger/domain/Currency.java
@@ -3,17 +3,34 @@
  */
 package com.qhc.steigenberger.domain;
 
-import lombok.Getter;
-import lombok.Setter;
 
 /**
  * @author wang@dxc.com
  *
  */
-@Setter
-@Getter
+
 public class Currency {
 	private String code;
 	private String name;
 	private double rate;
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getRate() {
+		return rate;
+	}
+	public void setRate(double rate) {
+		this.rate = rate;
+	}
+	
+	
 }
