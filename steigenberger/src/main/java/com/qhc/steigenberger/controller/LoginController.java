b1d2fba490c82b57fb721b21dfbe73c6b4524759
wangch <chunzhao.wang@carrier.com>
2019-11-07 17:15:54 +0800

    update 5

--- steigenberger/src/main/java/com/qhc/steigenberger/controller/LoginController.java
+++ steigenberger/src/main/java/com/qhc/steigenberger/controller/LoginController.java
@@ -12,6 +12,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.qhc.steigenberger.domain.User;
 import com.qhc.steigenberger.service.UserService;
 
 @Controller
@@ -40,7 +42,14 @@ public class LoginController extends BaseController{
     	String userName = request.getParameter("username");//AD域认证，用户的登录UserName
     	String password = request.getParameter("password");//AD域认证，用户的登录PassWord
     	try {
-    		int isActive = userService.selectUserIdentity(userName).getIsActive();
+    		User user  = userService.selectUserIdentity(userName);
+    		if(user == null) {
+    			modelAndView.addObject("userName",userName);
+        		modelAndView.addObject("msg","没有该用户!");
+        		modelAndView.setViewName("login");
+        		return modelAndView;
+    		}
+    		int isActive = user.getIsActive();
     		
     		if(userName=="" || password=="") {
         		modelAndView.addObject("msg","用户名或密码不能为空!");
@@ -57,11 +66,11 @@ public class LoginController extends BaseController{
         		String host = "utccgl.com";//AD域IP，必须填写正确
             	String domain = "utccgl\\";//域名后缀，
             	String url = new String("ldap://" + host );//
-            	String user = userName.indexOf(domain) > 0 ? userName : domain+userName;//网上有别的方法，但是在我这儿都不好使，建议这么使用
+            	String userN = userName.indexOf(domain) > 0 ? userName : domain+userName;//网上有别的方法，但是在我这儿都不好使，建议这么使用
             	Hashtable env = new Hashtable();//实例化一个Env
             	DirContext ctx = null;
             	env.put(Context.SECURITY_AUTHENTICATION, "simple");//LDAP访问安全级别(none,simple,strong),一种模式，这么写就行
-            	env.put(Context.SECURITY_PRINCIPAL, user); //用户名
+            	env.put(Context.SECURITY_PRINCIPAL, userN); //用户名
             	env.put(Context.SECURITY_CREDENTIALS, password);//密码
             	env.put(Context.INITIAL_CONTEXT_FACTORY,"com.sun.jndi.ldap.LdapCtxFactory");// LDAP工厂类
             	env.put(Context.PROVIDER_URL, url);//Url
