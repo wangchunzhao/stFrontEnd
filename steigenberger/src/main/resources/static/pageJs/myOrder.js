d4a2f91a5b32ac9698846e1f920c9179ee4c794d
wangch <chunzhao.wang@carrier.com>
2019-11-12 18:46:41 +0800

    update 4

--- steigenberger/src/main/resources/static/pageJs/myOrder.js
+++ steigenberger/src/main/resources/static/pageJs/myOrder.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
         $('#reservation').daterangepicker(
         		{
         		autoUpdateInput:true,
-        		startDate: moment().subtract('days', 6),  //这里配置的起止时间将会决定在ranges中默认选中哪个时间段
+        		startDate: moment().subtract('days', 7),  //这里配置的起止时间将会决定在ranges中默认选中哪个时间段
         		endDate: moment()
         		}, 
         		function(start, end, label) {
@@ -27,6 +27,7 @@ $(function () {
     var oTable = new TableInit();
     oTable.Init();
     InitSapSalesOffice();
+    InitTime();
 });
 
 var TableInit = function () {
@@ -249,8 +250,32 @@ function BuildAreaOption(name,code){
 	return "<option value='"+code+"'>"+name+"</option>";
 }
 
-
-
+function InitTime(){
+	var endDate = new Date();
+	var startDate = new Date();
+	startDate.setTime(endDate.getTime() - 1000*3600*24*7);
+	$("#reservation").val(dateFtt("yyyy-MM-dd", startDate)+" - "+dateFtt("yyyy-MM-dd", endDate));
+	$("#reservation1").val(dateFtt("yyyy-MM-dd", startDate)+" - "+dateFtt("yyyy-MM-dd", endDate));
+	
+}
+//时间格式化处理
+function dateFtt(fmt, date) { //author: meizz   
+    var o = {
+        "M+": date.getMonth() + 1, //月份   
+        "d+": date.getDate(), //日   
+        "h+": date.getHours(), //小时   
+        "m+": date.getMinutes(), //分   
+        "s+": date.getSeconds(), //秒   
+        "q+": Math.floor((date.getMonth() + 3) / 3), //季度   
+        "S": date.getMilliseconds() //毫秒   
+    };
+    if(/(y+)/.test(fmt))
+        fmt = fmt.replace(RegExp.$1, (date.getFullYear() + "").substr(4 - RegExp.$1.length));
+    for(var k in o)
+        if(new RegExp("(" + k + ")").test(fmt))
+            fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+    return fmt;
+}
 
 
 
