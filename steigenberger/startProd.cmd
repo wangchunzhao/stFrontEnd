8f1dc0944bb902c34316094b74dc9f5897d33e22
Suzuwei <zuwei.su@dxc.com>
2019-12-31 12:50:12 +0800

    修改启动脚本

--- steigenberger/startProd.cmd
+++ steigenberger/startProd.cmd
@@ -1,2 +1,2 @@
 title Selling Tools
-java -Xmx2048m -Xms1024m -jar st.jar --server.port =8880 --spring.profiles.active=prod
+java -Xmx2048m -Xms1024m -jar st.jar -Dserver.port=8880 -Dspring.profiles.active=prod
--- steigenberger/startTest.cmd
+++ steigenberger/startTest.cmd
@@ -1,2 +1,2 @@
 title Selling Tools
-java -Xmx2048m -Xms1024m -jar st.jar --server.port =8880 --spring.profiles.active=test
+java -Xmx2048m -Xms1024m -jar st.jar -Dserver.port =8880 -Dspring.profiles.active=test
