b8149f3b9c8993967a520b304ed979ba22ab20b1
Suzuwei <zuwei.su@dxc.com>
2020-06-02 09:27:48 +0800

    订单增加下推SAP待确认状态

--- steigenberger/src/main/java/com/qhc/steigenberger/domain/Order.java
+++ steigenberger/src/main/java/com/qhc/steigenberger/domain/Order.java
@@ -21,6 +21,7 @@ public class Order {
 	public final static String ORDER_STATUS_BPM = "04"; // 提交到BPM
 	public final static String ORDER_STATUS_APPROVED = "05"; // BPM审批通过
 	public final static String ORDER_STATUS_APPROVED_UPDATE = "06"; // 订单变更BPM审批通过
+    public final static String ORDER_STATUS_SAP_TEMP = "08"; // 下推SAP待确认
 	public final static String ORDER_STATUS_SAP = "09"; // 已下推SAP
 	public final static String ORDER_STATUS_REJECT = "10"; // Selling Tool驳回
 	public final static String ORDER_STATUS_REJECT_BPM = "11"; // BPM驳回
